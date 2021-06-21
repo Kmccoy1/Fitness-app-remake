6d50f0ba7cd4da718f47529a8de9bc35c500b426
Khalil McCoy <mccoy.khalil@gmail.com>
2021-06-20 20:48:48 -0500

    Initial Commit

---
+++ Fitness App Remake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fitness App Remake
+//
+//  Created by Khalil McCoy on 6/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
